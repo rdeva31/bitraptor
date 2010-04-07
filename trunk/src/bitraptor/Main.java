90edaa4035eca8407fb011bf4c22cd57735cf7bf
r.deva31 <r.deva31@39a67de1-d755-ba7d-ddfa-c3a546b0af34>
2010-04-07 00:03:57 +0000

    Partially implemented

--- trunk/src/bitraptor/Main.java
+++ trunk/src/bitraptor/Main.java
@@ -5,6 +5,10 @@ import java.io.InputStreamReader;
 import java.io.File;
 import java.io.FileInputStream;
 import org.klomp.snark.bencode.BDecoder;
+import org.klomp.snark.bencode.BEValue;
+import org.klomp.snark.bencode.InvalidBEncodingException;
+import java.util.Map;
+import java.util.List;
 
 public class Main {
 
@@ -79,6 +83,13 @@ public class Main {
 	*/
 	private static void handleSteal(File f)
 	{
+		//Variables detailing the attributes of the torrent and the file to be downloaded
+		String announceUrl = null;
+		List<List> announceLists = new java.util.ArrayList<List>();
+		long creationDate = 0;
+		String comment = null, createdBy = null, encoding = null;
+		Info info = new Info();;
+		
 		if (f == null)
 			return;
 		else if (!f.exists())
@@ -97,20 +108,89 @@ public class Main {
 			return;
 		}
 		
-		
+		/* Begin parsing the torrent file */
 		BDecoder decoder = null;
-		
+		BEValue value = null;
 		try
 		{
-			new BDecoder(new FileInputStream(f));
+			decoder = new BDecoder(new FileInputStream(f));
 		}
 		catch (java.io.FileNotFoundException e) //should never happen since f exists and is readable
 		{
 			System.out.println("Ok so this error shouldn't have happened.  I screwed up.  Are you sure some other process didn't delete the file in the past few microseconds?");
 			System.out.println(e);
+			return;
 		}
 		
-		//TODO write code to download torrent
+		
+		try
+		{
+			while ((value = decoder.bdecode()) != null)
+			{
+				String field = null;
+				field = new String(value.getBytes());
+			
+			
+				if (field.equalsIgnoreCase("info"))
+				{
+					Map infoDictionary = bdecoder.bdecode().getMap();
+					info.pieceLength = (long)infoDictionary.get("piece length");
+					
+					if (infoDictionary.containsKey("private"))
+						info.privateTorrent = (infoDictionary.get("private") == 1) ? true : false;
+					else
+						info.privateTorrent = false;
+						
+					info.pieces = (byte []) infoDictionary.get("pieces");
+				}
+				else if (field.equalsIgnoreCase("announce"))
+				{
+					announceUrl = new String(decoder.bdecode().getBytes());
+				}
+				else if (field.equalsIgnoreCase("announce-list"))
+				{
+					while(decoder.getNextIndicator() != 'l')
+						announceLists.add(decoder.bdecode().getList());
+				}
+				else if (field.equalsIgnoreCase("creation-date"))
+					creationDate = decoder.bdecode().getLong();
+				else if (field.equalsIgnoreCase("comment"))
+					comment = new String(decoder.bdecode().getBytes());
+				else if (field.equalsIgnoreCase("created-by"))
+					createdBy = new String(decoder.bdecode().getBytes());
+				else if (field.equalsIgnoreCase("encoding"))
+					encoding = new String(decoder.bdecode().getBytes());
+				else
+					throw new InvalidBEncodingException("unknown field");
+				
+				
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("The torrent file is improperly constructed...just like yo mama");
+			e.printStackTrace();
+			return;
+		}
+		/* End parsing the torrent file */
+		System.out.println("announceurl : " + announceUrl);
+		System.out.println("announceurl-lists : " + announceLists.toString());
+		System.out.println("Torrent created by " + createdBy + " on " + creationDate +" with encoding" + encoding);
+		System.out.println(comment);
 	}
-   
+	
+	
+	private class Info
+	{
+		long pieceLength;
+		byte [] pieces;
+		boolean privateTorrent;
+	}
+	
+	private class SingleFileInfo extends Info
+	{
+		String info;
+		long fileLength;
+		byte md5sum[];
+	}
 }
