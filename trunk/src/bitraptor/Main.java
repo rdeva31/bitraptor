6a2b15ec71f8354f68b1546141f8734e5e299c00
r.deva31 <r.deva31@39a67de1-d755-ba7d-ddfa-c3a546b0af34>
2010-04-07 02:15:13 +0000

    Completely implemented the .torrent file parser

--- trunk/src/bitraptor/Main.java
+++ trunk/src/bitraptor/Main.java
@@ -9,6 +9,7 @@ import org.klomp.snark.bencode.BEValue;
 import org.klomp.snark.bencode.InvalidBEncodingException;
 import java.util.Map;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -38,7 +39,7 @@ public class Main {
 			command = commandFull[0];
 			
 			if (command.equals("help"))
-				handleHelp();
+				new Main().handleHelp();
 			else if (command.equals("exit"))
 				return;
 			else if (command.equals("steal"))
@@ -54,7 +55,7 @@ public class Main {
 					continue;
 				}
 				
-				handleSteal(new File(commandFull[1]));
+				new Main().handleSteal(new File(commandFull[1]));
 			}
 			else
 				System.out.println("im a computer and what is this");
@@ -64,7 +65,7 @@ public class Main {
 	/**
 		Helper function for the "help" command used by main()
 	*/
-	private static void handleHelp()
+	private void handleHelp()
 	{
 		String str = "";
 		str += "Available commands are:\n";
@@ -81,14 +82,14 @@ public class Main {
 		
 		@param f - the .torrent file to read from
 	*/
-	private static void handleSteal(File f)
+	private void handleSteal(File f)
 	{
 		//Variables detailing the attributes of the torrent and the file to be downloaded
 		String announceUrl = null;
 		List<List> announceLists = new java.util.ArrayList<List>();
 		long creationDate = 0;
 		String comment = null, createdBy = null, encoding = null;
-		Info info = new Info();;
+		SingleFileInfo info = new SingleFileInfo();
 		
 		if (f == null)
 			return;
@@ -122,47 +123,53 @@ public class Main {
 			return;
 		}
 		
-		
 		try
 		{
-			while ((value = decoder.bdecode()) != null)
+			Map<String, BEValue> torrentFileMappings = decoder.bdecode().getMap();
+			Set<String> fields = torrentFileMappings.keySet();
+			
+			for (String field : fields) //handle each field
 			{
-				String field = null;
-				field = new String(value.getBytes());
-			
-			
 				if (field.equalsIgnoreCase("info"))
 				{
-					Map infoDictionary = bdecoder.bdecode().getMap();
-					info.pieceLength = (long)infoDictionary.get("piece length");
+					Map<String, BEValue> infoDictionary = torrentFileMappings.get(field).getMap();
+					info.pieceLength = infoDictionary.get("piece length").getInt();
 					
 					if (infoDictionary.containsKey("private"))
-						info.privateTorrent = (infoDictionary.get("private") == 1) ? true : false;
+						info.privateTorrent = (infoDictionary.get("private").getInt() == 1) ? true : false;
 					else
 						info.privateTorrent = false;
 						
-					info.pieces = (byte []) infoDictionary.get("pieces");
+					info.pieces = infoDictionary.get("pieces").getBytes();;
+					
+					info.name = new String(infoDictionary.get("name").getBytes());
+					info.fileLength = infoDictionary.get("length").getInt();
+					
+					if (infoDictionary.containsKey("md5sum"))
+						info.md5sum = infoDictionary.get("md5sum").getBytes();
+					
 				}
 				else if (field.equalsIgnoreCase("announce"))
 				{
-					announceUrl = new String(decoder.bdecode().getBytes());
+					announceUrl = new String(torrentFileMappings.get(field).getBytes());
 				}
 				else if (field.equalsIgnoreCase("announce-list"))
 				{
-					while(decoder.getNextIndicator() != 'l')
-						announceLists.add(decoder.bdecode().getList());
+					//while(decoder.getNextIndicator() != 'l')
+					//	announceLists.add(decoder.bdecode().getList());
+					//throw new Exception("Need to implement announce-list");
 				}
 				else if (field.equalsIgnoreCase("creation-date"))
-					creationDate = decoder.bdecode().getLong();
+					creationDate = torrentFileMappings.get(field).getLong();
 				else if (field.equalsIgnoreCase("comment"))
-					comment = new String(decoder.bdecode().getBytes());
+					comment = new String(torrentFileMappings.get(field).getBytes());
 				else if (field.equalsIgnoreCase("created-by"))
-					createdBy = new String(decoder.bdecode().getBytes());
+					createdBy = new String(torrentFileMappings.get(field).getBytes());
 				else if (field.equalsIgnoreCase("encoding"))
-					encoding = new String(decoder.bdecode().getBytes());
+					encoding = new String(torrentFileMappings.get(field).getBytes());
 				else
-					throw new InvalidBEncodingException("unknown field");
-				
+					;//throw new InvalidBEncodingException("unknown field: " + field); //don't throw this because apparently torrent files have fields
+																						//not specified in the official docs
 				
 			}
 		}
@@ -175,22 +182,36 @@ public class Main {
 		/* End parsing the torrent file */
 		System.out.println("announceurl : " + announceUrl);
 		System.out.println("announceurl-lists : " + announceLists.toString());
-		System.out.println("Torrent created by " + createdBy + " on " + creationDate +" with encoding" + encoding);
-		System.out.println(comment);
+		System.out.println("Torrent created by " + createdBy + " on " + creationDate +" with encoding " + encoding);
+		System.out.println("Comments: " + comment);
+		
+		System.out.println("info:");
+		System.out.println("piece length : " + info.pieceLength);
+		System.out.println("Is private? " + info.privateTorrent);
+		System.out.print("File name: " + info.name + "(" + info.fileLength + " bytes)");
+		if (info.md5sum == null)
+			System.out.println(" with no md5sum");
+		else
+			System.out.println(" with md5sum " + info.md5sum);
+		System.out.println("Have hashes for " + info.pieces.length + " pieces");
+		
+		//TODO: matt, start working here.  The printlns above are just for reference.  Everything you need is in the variables that 
+		//are println'ed.  Some of them might be null, so be careful with that shit
+		
 	}
 	
 	
 	private class Info
 	{
-		long pieceLength;
+		int pieceLength;
 		byte [] pieces;
 		boolean privateTorrent;
 	}
 	
 	private class SingleFileInfo extends Info
 	{
-		String info;
-		long fileLength;
+		String name;
+		int fileLength;
 		byte md5sum[];
 	}
 }
