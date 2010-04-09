76b43e069c47520fe4e7c8bdc2c5194c36166eed
r.deva31 <r.deva31@39a67de1-d755-ba7d-ddfa-c3a546b0af34>
2010-04-09 01:58:47 +0000

    Info.java: Broke up into two sub classes SingleFileInfo.java and MultiFileInfo.java.  Instead of public fields, added some getters and settes
    Torrent.java: Updated to reflect changes in Info.java
    
    Main.java: Added Multifile support and AnnounceList support (parsing)
    
    Peer.java:  Added Ipaddr and port to peer properties.

--- trunk/src/bitraptor/Main.java
+++ trunk/src/bitraptor/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		Starts the BitRaptor program.  No arguments required.
 	 */
 	public static void main(String[] args) {
-		System.out.println("BitRaptor -- Takes a BITE out of crime");
+		System.out.println("BitRaptor -- Makes your penis longer");
 		System.out.println("(Type 'help' to see available commands)");
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -112,7 +112,7 @@ public class Main {
 		//Begin parsing the torrent file
 		BDecoder decoder = null;
 		BEValue value = null;
-		Info info = new Info();
+		Info info = new SingleFileInfo(); //declare as a singlefile info, change later
 		
 		try
 		{
@@ -135,92 +135,161 @@ public class Main {
 				//Info
 				if (field.equalsIgnoreCase("info"))
 				{
+					
 					Map<String, BEValue> infoDictionary = torrentFileMappings.get(field).getMap();
-					
-					//Piece Length
-					info.pieceLength = infoDictionary.get("piece length").getInt();
-					
-					//Private
-					if (infoDictionary.containsKey("private"))
-						info.privateTorrent = (infoDictionary.get("private").getInt() == 1) ? true : false;
+
+					if (infoDictionary.containsKey("files")) //multiple files mode
+					{
+						info = new MultiFileInfo(info);
+						MultiFileInfo infoAlias = (MultiFileInfo)info;
+						
+						infoAlias.setDirectory(new String(infoDictionary.get("name").getBytes()));
+						infoAlias.setFiles(new ArrayList<SingleFileInfo>());
+						
+						List<BEValue> fileDictionaries = infoDictionary.get("files").getList();
+						for (BEValue fileDictionary : fileDictionaries)
+						{
+							Map<String, BEValue> fileDictionaryMap = fileDictionary.getMap();
+							SingleFileInfo fileInfo = new SingleFileInfo();
+
+							//name and path
+							List<BEValue> paths = fileDictionaryMap.get("path").getList();
+							String filePath = null;
+							
+							for (int c = 0; c < paths.size(); ++c)
+							{
+								if (c == 0)
+									filePath = new String(paths.get(c).getBytes());
+								else if (c != paths.size() - 1)
+									filePath += "/" + new String(paths.get(c).getBytes());
+							}
+
+							fileInfo.setName(filePath);
+
+
+							//file size
+							fileInfo.setFileLength(fileDictionaryMap.get("length").getInt());
+
+							//md5sum of file
+							if (fileDictionaryMap.containsKey("md5sum"))
+								fileInfo.setMd5sum(fileDictionaryMap.get("md5sum").getBytes());
+
+							//add to file to directory
+							infoAlias.getFiles().add(fileInfo);
+						}
+						
+					}
+					else if (infoDictionary.containsKey("length"))	//single file mode
+					{
+						info = new SingleFileInfo(info);
+						SingleFileInfo infoAlias = (SingleFileInfo)info;
+
+
+						//Name
+						infoAlias.setName(new String(infoDictionary.get("name").getBytes()));
+
+						//Length
+						infoAlias.setFileLength(infoDictionary.get("length").getInt());
+
+						//MD5 Checksum
+						if (infoDictionary.containsKey("md5sum"))
+							infoAlias.setMd5sum(infoDictionary.get("md5sum").getBytes());
+
+					}
 					else
-						info.privateTorrent = false;
-						
-					//Pieces
-					info.pieces = infoDictionary.get("pieces").getBytes();;
+						throw new Exception("Invalid torrent file.  info doesn't contain files or length");
 					
-					//Name
-					info.name = new String(infoDictionary.get("name").getBytes());
-					
-					//Length
-					info.fileLength = infoDictionary.get("length").getInt();
-					
-					//MD5 Checksum
-					if (infoDictionary.containsKey("md5sum"))
-						info.md5sum = infoDictionary.get("md5sum").getBytes();
-						
 					//Hash of 'info' field
-					info.infoHash = decoder.get_special_map_digest();
-					
+					info.setInfoHash(decoder.get_special_map_digest());
+
+					//These are common to both SingleFileInfo and MultifileInfo
+					//Piece Length
+					info.setPieceLength(infoDictionary.get("piece length").getInt());
+
+					//Privateinfo.announceUrl
+					if (infoDictionary.containsKey("private"))
+						info.setPrivateTorrent((infoDictionary.get("private").getInt() == 1) ? true : false);
+					else
+						info.setPrivateTorrent(false);
+
+					//Pieces
+					info.setPieces(infoDictionary.get("pieces").getBytes());
 				}
 				//Announce
 				else if (field.equalsIgnoreCase("announce"))
 				{
-					info.announceUrl = new URL(new String(torrentFileMappings.get(field).getBytes()));
+					if (info.getAnnounceUrls() == null)
+						info.setAnnounceUrls(new ArrayList<URL>());
+					info.getAnnounceUrls().add(0, new URL(new String(torrentFileMappings.get(field).getBytes()))); //always want announce URL to be top choice
 				}
 				//Announce List
 				else if (field.equalsIgnoreCase("announce-list"))
 				{
-					//TODO: Add support for announce list
+					List<BEValue> announceLists = torrentFileMappings.get(field).getList();
+					
+					if (info.getAnnounceUrls() == null)
+						info.setAnnounceUrls(new ArrayList<URL>());
+						
+					for (BEValue b : announceLists)
+					{
+						List<BEValue> l = b.getList(); 
+						for (BEValue announceUrl : l)
+						{
+							URI u = new URI(new String(announceUrl.getBytes()));
+							if (u.getScheme().equalsIgnoreCase("http"))
+								info.getAnnounceUrls().add(u.toURL());
+						}
+					}
 				}
 				//Creation Date
 				else if (field.equalsIgnoreCase("creation date"))
 				{
-					info.creationDate = torrentFileMappings.get(field).getLong();
+					info.setCreationDate(torrentFileMappings.get(field).getLong());
 				}
 				//Comment
 				else if (field.equalsIgnoreCase("comment"))
 				{
-					info.comment = new String(torrentFileMappings.get(field).getBytes());
+					info.setComment(new String(torrentFileMappings.get(field).getBytes()));
 				}
 				//Created-By
 				else if (field.equalsIgnoreCase("created by"))
 				{
-					info.createdBy = new String(torrentFileMappings.get(field).getBytes());
+					info.setCreatedBy(new String(torrentFileMappings.get(field).getBytes()));
 				}
 				//Encoding
 				else if (field.equalsIgnoreCase("encoding"))
 				{
-					info.encoding = new String(torrentFileMappings.get(field).getBytes());
+					info.setEncoding(new String(torrentFileMappings.get(field).getBytes()));
 				}
 			}
 		}
 		catch (Exception e)
 		{
 			System.out.println("ERROR: Invalid torrent file");
+			e.printStackTrace();
 			return;
 		}
 		
 		////////////////////////////////////////////////////////////////
 		////////////////////////////////////////////////////////////////
-		System.out.println("AnnounceURL : " + info.announceUrl);
-		System.out.println("AnnounceURL-Lists : " + info.announceLists.toString());
-		System.out.println("Torrent Created By " + info.createdBy + " on " + info.creationDate +" with encoding " + info.encoding);
-		System.out.println("Comments: " + info.comment);
+		System.out.println("AnnounceURL : " + info.getAnnounceUrls());
+		System.out.println("Torrent Created By " + info.getCreatedBy() + " on " + info.getCreationDate() +" with encoding " + info.getEncoding());
+		System.out.println("Comments: " + info.getComment());
 		System.out.println("Info:");
-		System.out.println("\tPiece Length : " + info.pieceLength);
-		System.out.println("\tPrivate: " + info.privateTorrent);
-		System.out.print("\tFile Name: " + info.name + "(" + info.fileLength + " bytes)");
-		if (info.md5sum == null)
-		{
-			System.out.println(" with NO md5sum");
-		}
-		else
-		{
-			System.out.println(" with md5sum " + info.md5sum);
-		}
-		System.out.println("\tHashes for " + info.pieces.length + " pieces");
-		System.out.println("\tInfo hash: " + info.infoHash);
+		System.out.println("\tPiece Length : " + info.getPieceLength());
+		System.out.println("\tPrivate: " + info.isPrivateTorrent());
+		System.out.println(info.toString());
+		//System.out.print("\tFile Name: " + info.name + "(" + info.fileLength + " bytes)");
+//		if (info.getMd5sum() == null)
+//		{
+//			System.out.println(" with NO md5sum");
+//		}
+//		else
+//		{
+//			System.out.println(" with md5sum " + info.ge);
+//		}
+//		System.out.println("\tHashes for " + info.getPieces().length + " pieces");
+//		System.out.println("\tInfo hash: " + info.getInfoHash());
 		////////////////////////////////////////////////////////////////
 		////////////////////////////////////////////////////////////////
 		
